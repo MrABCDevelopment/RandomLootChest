5eec0ab4eb17413216aa69a4a7bc49156e53c967
BuildTools <MrABCDevelopment@gmail.com>
2022-03-21 18:52:12 +0100

    Added new functions

--- src/main/java/me/mrsandking/github/randomlootchest/manager/GameManager.java
+++ src/main/java/me/mrsandking/github/randomlootchest/manager/GameManager.java
@@ -29,36 +29,41 @@ public class GameManager {
         itemStacks = new ArrayList<>();
         for(String key : plugin.getConfigManager().getConfig("config.yml").getStringList("gamechest-items")) {
             String[] params = key.split(":");
-            if(params[0].contains("potion")) {
-                String[] strings = params[0].split(",");
-                RandomItem randomItem = new RandomItem(Util.getPotion(strings[1].toUpperCase(), Integer.parseInt(strings[2]), false, Integer.parseInt(params[1])), Double.parseDouble(params[2]));
-                itemStacks.add(randomItem);
-            } else if(params[0].equalsIgnoreCase("enchanted_golden_apple")) {
-                ItemStack itemStack = null;
-                try {
-                    itemStack = new ItemStack(Material.GOLDEN_APPLE, 1, (byte) 1);
-                } catch (Exception e) {
-                    itemStack = new ItemStack(Material.getMaterial(params[0].toUpperCase()));
-                }
-                itemStack.setAmount(Integer.parseInt(params[1]));
-                RandomItem randomItem = new RandomItem(itemStack, Double.parseDouble(params[2]));
-                itemStacks.add(randomItem);
-            } else if(params[0].contains("splash_potion")) {
-                String[] strings = params[0].split(",");
-                RandomItem randomItem = new RandomItem(Util.getPotion(strings[0], Integer.parseInt(strings[1]), true, Integer.parseInt(params[1])), Double.parseDouble(params[2]));
-                itemStacks.add(randomItem);
-            } else {
-                ItemStack itemStack = new ItemStack(Material.getMaterial(params[0].toUpperCase()));
-                itemStack.setAmount(Integer.parseInt(params[1]));
-                if (params.length == 4) {
-                    String[] enchantments = params[3].split(";");
-                    for (String enchantment : enchantments) {
-                        String[] p = enchantment.split(",");
-                        itemStack.addUnsafeEnchantment(Enchantment.getByName(p[0].toUpperCase()), Integer.parseInt(p[1]));
+            try {
+                if (params[0].contains("potion")) {
+                    String[] strings = params[0].split(",");
+                    RandomItem randomItem = new RandomItem(Util.getPotion(strings[1].toUpperCase(), Integer.parseInt(strings[2]), false, Integer.parseInt(params[1])), Double.parseDouble(params[2]));
+                    itemStacks.add(randomItem);
+                } else if (params[0].equalsIgnoreCase("enchanted_golden_apple")) {
+                    ItemStack itemStack = null;
+                    try {
+                        itemStack = new ItemStack(Material.GOLDEN_APPLE, 1, (byte) 1);
+                    } catch (Exception e) {
+                        itemStack = new ItemStack(Material.getMaterial(params[0].toUpperCase()));
                     }
+                    itemStack.setAmount(Integer.parseInt(params[1]));
+                    RandomItem randomItem = new RandomItem(itemStack, Double.parseDouble(params[2]));
+                    itemStacks.add(randomItem);
+                } else if (params[0].contains("splash_potion")) {
+                    String[] strings = params[0].split(",");
+                    RandomItem randomItem = new RandomItem(Util.getPotion(strings[0], Integer.parseInt(strings[1]), true, Integer.parseInt(params[1])), Double.parseDouble(params[2]));
+                    itemStacks.add(randomItem);
+                } else {
+                    ItemStack itemStack = new ItemStack(Material.getMaterial(params[0].toUpperCase()));
+                    itemStack.setAmount(Integer.parseInt(params[1]));
+                    if (params.length == 4) {
+                        String[] strings = params[3].split(";");
+                        for (String enchantment : strings) {
+                            String[] p = enchantment.split(",");
+                            itemStack.addUnsafeEnchantment(Enchantment.getByName(p[0].toUpperCase()), Integer.parseInt(p[1]));
+                        }
+                    }
+                    RandomItem randomItem = new RandomItem(itemStack, Double.parseDouble(params[2]));
+                    itemStacks.add(randomItem);
                 }
-                RandomItem randomItem = new RandomItem(itemStack, Double.parseDouble(params[2]));
-                itemStacks.add(randomItem);
+            } catch (NullPointerException e) {
+                Bukkit.getConsoleSender().sendMessage(ChatColor.RED+"There is an error with '"+params[0]+"' in config.yml");
+                continue;
             }
         }
     }
@@ -69,12 +74,24 @@ public class GameManager {
         for(RandomItem randomItem : getItems()) {
             if(counter == plugin.getConfigManager().getConfig("config.yml").getInt("max-items")) continue;
             if(Util.chance(randomItem.getChance())) {
-                inventory.setItem(Util.randomSlot(inventory.getSize()), randomItem.getItemStack());
-                counter++;
+                int max = plugin.getConfigManager().getConfig("config.yml").getInt("max-items-in-same-type");
+                if(max > 0) {
+                    int i = 0;
+                    for(int x = 0; x<inventory.getSize(); x++) {
+                        if(inventory.getItem(x).getType() == randomItem.getItemStack().getType()) {
+                            i++;
+                            inventory.setItem(Util.randomSlot(inventory.getSize()), randomItem.getItemStack());
+                            counter++;
+                        }
+                        if(i >= max) continue;
+                    }
+                } else {
+                    inventory.setItem(Util.randomSlot(inventory.getSize()), randomItem.getItemStack());
+                    counter++;
+                }
             }
         }
         player.openInventory(inventory);
-        //player.playSound(player.getLocation(), Sound.CHEST_OPEN, 2f, 2f);
     }
 
     public List<RandomItem> getItems() {
